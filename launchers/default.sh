c22964dfaa2d5bb925fc1061aa7601baa4bf9158
vivekvelachhawala <vivekvelachhawala@gmail.com>
2024-09-11 17:15:18 +0200

    added ood and launchers

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,8 +13,8 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
 
+rosrun ros_package camera_reader_node.py
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
